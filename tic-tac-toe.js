a92b3edcfbe7701bc5f6117821082def819471bb
Ashley Chung - Fah <ashliichung19@gmail.com>
2024-10-25 14:22:18 -0500

    Exercise 2 - Added X or O on click

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll("#board > div").forEach(div => div.classList.add("square"));
 });
+
+
+let turn = "X";
+document.querySelectorAll("#board > div").forEach(square => {
+    square.addEventListener("click", () => {
+        if (square.textContent === "") {
+            square.textContent = turn;
+            square.classList.add(turn);
+            turn = turn === "X" ? "O" : "X";
+        }
+    });
+});
